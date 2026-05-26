fe0a2e16033b0251bfd64aee68a6382e9e33a79c
SwathiSalla <swathisalla25@gmail.com>
2026-05-26 14:42:49 +0530

    Added beginner Python programs

---
+++ calculator.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+choice=input("Enter choice(1/2/3/4): ")
+if choice=='1':
+    print(a,"+",b,"=",a+b)
+elif choice=='2':
+    print(a,"-",b,"=",a-b)
+elif choice=='3':
+    print(a,"*",b,"=",a*b)
+elif choice=='4':
+    if b!=0:
+        print(a,"/",b,"=",a/b)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid input")
